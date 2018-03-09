37775a3da45e4bbace169e53b74fadcfd2fbe1ff
Beth Barnes <bethcbarnes@outlook.com>
2018-03-08 21:32:17 -0500

    tried adding a 3D obj not yet working

--- Desktop/ViroSample/js/MainScene.js
+++ Desktop/ViroSample/js/MainScene.js
@@ -35,8 +35,8 @@ var MainScene = createReactClass({
           <Viro360Image source={require("./portal_res/360_island.jpg")} />
         </ViroPortalScene>
 
-        <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={()=>{}}>
-          <ViroPortal position={[-.8, 0, -1]} scale={[.1, .1, .1]} rotation={[0, 45, 0]}>
+        {/* <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={()=>{}}>
+          <ViroPortal position={[1, 0, -1]} scale={[.1, .1, .1]} rotation={[0, 45, 0]}>
             <Viro3DObject source={require('./portal_res/portal_ship/portal_ship.vrx')}
               resources={[require('./portal_res/portal_ship/portal_ship_diffuse.png'),
                           require('./portal_res/portal_ship/portal_ship_normal.png'),
@@ -44,6 +44,21 @@ var MainScene = createReactClass({
               type="VRX"/>
           </ViroPortal>
           <Viro360Video source={require("./portal_res/trippy_vid.mp4")} loop={true} />
+        </ViroPortalScene> */}
+
+        <ViroPortalScene>
+            <ViroPortal position={[1, 0, -1]} scale={[.5, .5, .5]}>
+              <Viro3DObject
+                  source={require("../Wolfy")}
+                  // resources={[require('../portal_res/Wolf__done_obj.mtl')
+                  //   ]}
+                  highAccuracyGaze={true}
+                  position={[1, 3, -5]}
+                  scale={[2, 2, 2]}
+                  rotation={[45, 0, 0]}
+                  type="OBJ"
+                  transformBehaviors={["billboard"]}/>
+                          </ViroPortal>
         </ViroPortalScene>
 
       </ViroARScene>
