d49395aa62a2eec16a56798819941aabc85e0be1
Dan Ryan <djxr35@gmail.com>
2018-03-09 13:27:35 -0500

    changed size of portls, rotated portals and created map to set up 4 portals

--- Desktop/ViroSample/App.js
+++ Desktop/ViroSample/App.js
@@ -32,11 +32,9 @@ var sharedProps = {
 // Sets the default scene you want for AR and VR
 
 var InitialARScene = require('./js/MainScene');
-var InitialVRScene = require('./js/HelloWorldScene');
-// var InitialARScene = require('./js/ARPortals/MainScene');
+
 
 var UNSET = "UNSET";
-var VR_NAVIGATOR_TYPE = "VR";
 var AR_NAVIGATOR_TYPE = "AR";
 
 // This determines which type of experience to launch in, or UNSET, if the user should
@@ -53,7 +51,6 @@ export default class ViroSample extends Component {
     }
     this._getExperienceSelector = this._getExperienceSelector.bind(this);
     this._getARNavigator = this._getARNavigator.bind(this);
-    this._getVRNavigator = this._getVRNavigator.bind(this);
     this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
     this._exitViro = this._exitViro.bind(this);
   }
@@ -63,9 +60,8 @@ export default class ViroSample extends Component {
   render() {
     if (this.state.navigatorType == UNSET) {
       return this._getExperienceSelector();
-    } else if (this.state.navigatorType == VR_NAVIGATOR_TYPE) {
-      return this._getVRNavigator();
-    } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
+    }
+     else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
       return this._getARNavigator();
     }
   }
@@ -87,12 +83,6 @@ export default class ViroSample extends Component {
             <Text style={localStyles.buttonText}>AR</Text>
           </TouchableHighlight>
 
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={this._getExperienceButtonOnPress(VR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>VR</Text>
-          </TouchableHighlight>
         </View>
       </View>
     );
@@ -107,12 +97,12 @@ export default class ViroSample extends Component {
   }
 
   // Returns the ViroSceneNavigator which will start the VR experience
-  _getVRNavigator() {
-    return (
-      <ViroSceneNavigator {...this.state.sharedProps}
-        initialScene={{scene: InitialVRScene}} onExitViro={this._exitViro}/>
-    );
-  }
+  // _getVRNavigator() {
+  //   return (
+  //     <ViroSceneNavigator {...this.state.sharedProps}
+  //       initialScene={{scene: InitialVRScene}} onExitViro={this._exitViro}/>
+  //   );
+  // }
 
   // This function returns an anonymous/lambda function to be used
   // by the experience selector buttons
--- Desktop/ViroSample/js/MainScene.js
+++ Desktop/ViroSample/js/MainScene.js
@@ -17,50 +17,48 @@ import {
   Viro3DObject,
 } from 'react-viro';
 
+
 var createReactClass = require('create-react-class');
 var MainScene = createReactClass({
 
   render: function() {
+    var key = 0;
+    var arrToMap = [{position: [0, -0.5, -2] ,//front
+                      rotation:[0,0,0],
+                      image:require('./portal_res/360_island.jpg'),
+                    key: 1},
+                    {position: [2, -0.5, 0],//right
+                      rotation:[0,270,0],
+                      image:require('./portal_res/360_island.jpg'),
+                      key: 2},
+                    {position:[0, -0.5, 2],//back
+                      rotation:[0,180,0],
+                      image:require('./portal_res/360_island.jpg'),
+                      key: 3},
+                    {position:[-2, -0.5, 0],//left
+                      rotation:[0,90,0],
+                      image:require('./portal_res/360_island.jpg'),
+                    key: 4}]
+
     return (
       <ViroARScene>
       <ViroAmbientLight color="#ffffff" intensity={200}/>
-        <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={()=>{}}>
-          <ViroPortal position={[0, 0, -1]} scale={[.1, .1, .1]}>
-            <Viro3DObject source={require('./portal_res/portal_ship/portal_ship.vrx')}
-              resources={[require('./portal_res/portal_ship/portal_ship_diffuse.png'),
-                          require('./portal_res/portal_ship/portal_ship_normal.png'),
-                          require('./portal_res/portal_ship/portal_ship_specular.png')]}
-              type="VRX"/>
-          </ViroPortal>
-          <Viro360Image source={require("./portal_res/360_island.jpg")} />
-        </ViroPortalScene>
+         {
+           arrToMap.map(portal => {
+          return (
+              <ViroPortalScene key= {+portal.key} passable={true} dragType="FixedDistance" onDrag={()=>{}}>
+                  <ViroPortal position={portal.position} scale={[.2, .25, .1]} rotation= {portal.rotation}>
+                    <Viro3DObject source={require('./portal_res/portal_ship/portal_ship.vrx')}
+                      resources={[require('./portal_res/portal_ship/portal_ship_diffuse.png'),
+                                  require('./portal_res/portal_ship/portal_ship_normal.png'),
+                                  require('./portal_res/portal_ship/portal_ship_specular.png')]}
+                      // rotation= {portal.rotation}
+                      type="VRX"/>
+                  </ViroPortal>
+                  <Viro360Image source={portal.image} />
+          </ViroPortalScene>)})
+      }
 
-        {/* <ViroPortalScene passable={true} dragType="FixedDistance" onDrag={()=>{}}>
-          <ViroPortal position={[1, 0, -1]} scale={[.1, .1, .1]} rotation={[0, 45, 0]}>
-            <Viro3DObject source={require('./portal_res/portal_ship/portal_ship.vrx')}
-              resources={[require('./portal_res/portal_ship/portal_ship_diffuse.png'),
-                          require('./portal_res/portal_ship/portal_ship_normal.png'),
-                          require('./portal_res/portal_ship/portal_ship_specular.png')]}
-              type="VRX"/>
-          </ViroPortal>
-          <Viro360Video source={require("./portal_res/trippy_vid.mp4")} loop={true} />
-        </ViroPortalScene> */}
-
-        <ViroPortalScene>
-            <ViroPortal position={[1, 0, -1]} scale={[.5, .5, .5]}>
-              <Viro3DObject
-                  source={require('./portal_res/Male.OBJ')}
-                  // resources={[require('./portal_res/Wolf.c4d')
-                  //   ]}
-                  // highAccuracyGaze={true}
-                  position={[1, 3, -5]}
-                  // scale={[2, 2, 2]}
-                  rotation={[45, 0, 0]}
-                  type="OBJ"
-                  // transformBehaviors={["billboard"]}
-                  />
-                          </ViroPortal>
-        </ViroPortalScene>
 
       </ViroARScene>
     );
